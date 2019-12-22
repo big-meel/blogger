20308f0a8a6019db6202df595ca492f500d6185d
Jamz <big.meel@zoho.com>
2019-12-22 18:48:01 -0500

    Add Flash messages to Form Actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,18 +20,27 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         @article.save
+
+        flash.notice = "Created article '#{@article.title}'!"
+
         redirect_to article_path(@article)
     end
 
     def update
         @article = Article.find(params[:id])
         @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+
         redirect_to article_path(@article)
     end
 
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
+
+        flash.notice = "Article '#{@article.title}' has been Removed!"
+
         redirect_to articles_path
     end
 
